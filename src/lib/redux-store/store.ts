9fa21c1c5d868da4732ec082ad0fc95845e32d2b
Kaustavi Banerjee <kaustavi@itobuz.com>
2022-01-21 19:44:40 +0530

    localstorage implement

--- src/lib/redux-store/store.ts
+++ src/lib/redux-store/store.ts
@@ -1,11 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
 import listReducer from './reducers/listSlice';
 
+function saveToLocalStorage(state: object) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("itemList", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("itemList");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
+
 const store = configureStore({
   reducer: {
     list: listReducer
-  }
+  },
+  preloadedState: loadFromLocalStorage()
 });
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export type RootState = ReturnType<typeof store.getState>;
 
